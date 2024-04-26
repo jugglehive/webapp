4855566cba0e440fba9dd575f13e0d8255d76de3
GrittaGit <gritta.commercial@gmail.com>
2024-04-26 17:05:32 +0200

    Annotations @Data/ @Entity added where missing

--- backend/src/main/java/com/jugglehive/model/entity/CharachterSkills.java
+++ backend/src/main/java/com/jugglehive/model/entity/CharachterSkills.java
@@ -1,6 +1,7 @@
 package com.jugglehive.model.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +9,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import lombok.Data;
 
+@Entity
+@Data
 public class CharachterSkills {
 
     @Id
