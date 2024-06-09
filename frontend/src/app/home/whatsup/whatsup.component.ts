2ca9206c3de877e31cfdfa72dfdbcbe08a3d9bba
jugglehive <heatpeakstudio@gmail.com>
2024-06-09 18:06:40 +0200

    First company homepage draft

---
+++ frontend/src/app/home/whatsup/whatsup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsup',
+  templateUrl: './whatsup.component.html',
+  styleUrl: './whatsup.component.css'
+})
+export class WhatsupComponent {
+
+}
